d0b108861411bbf38c7cf01a4356106826233514
nikhyl06 <nikhilshankhwar2003@gmail.com>
2024-06-06 21:09:17 +0530

    ready to deploy

--- src/pages/Dashboard/GraphDisplay.jsx
+++ src/pages/Dashboard/GraphDisplay.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
-import DowntimeDay from './graphs/DowntimeDay';
-import DowntimeMonth from './graphs/DowntimeMonth';
-import SparePartsDay from './graphs/SparePartsDay';
-import SparePartsMonth from './graphs/SparePartsMonth';
+import React, { Suspense, lazy } from "react";
+
+const DowntimeDay = lazy(() => import("./graphs/DowntimeDay"));
+const DowntimeMonth = lazy(() => import("./graphs/DowntimeMonth"));
+const SparePartsDay = lazy(() => import("./graphs/SparePartsDay"));
+const SparePartsMonth = lazy(() => import("./graphs/SparePartsMonth"));
 
 const GraphDisplay = ({ activeTab, dateLimits }) => {
   return (
     <div className="graph-display">
-      {activeTab === 'DowntimeDay' && <DowntimeDay dateLimits={dateLimits} />}
-      {activeTab === 'DowntimeMonth' && <DowntimeMonth dateLimits={dateLimits} />}
-      {activeTab === 'SparePartsMonth' && <SparePartsMonth dateLimits={dateLimits} />}
-      {activeTab === 'SparePartsDay' && <SparePartsDay dateLimits={dateLimits} />}
+      {activeTab === "DowntimeDay" && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <DowntimeDay dateLimits={dateLimits} />
+        </Suspense>
+      )}
+      {activeTab === "DowntimeMonth" && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <DowntimeMonth dateLimits={dateLimits} />
+        </Suspense>
+      )}
+      {activeTab === "SparePartsMonth" && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <SparePartsDay dateLimits={dateLimits} />
+        </Suspense>
+      )}
+      {activeTab === "SparePartsDay" && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <SparePartsMonth dateLimits={dateLimits} />
+        </Suspense>
+      )}
     </div>
   );
 };
